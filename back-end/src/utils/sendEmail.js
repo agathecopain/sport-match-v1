bf6207b7d537da7b11fbadf0228da27bc6bab270
Agathe Boncompain <agathe.boncompain@gmail.com>
2025-11-02 19:09:47 +0100

    feat(sendEmail) : Brevo smtp config

--- back-end/src/utils/sendEmail.js
+++ back-end/src/utils/sendEmail.js
@@ -2,10 +2,10 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.SMTP_HOST,
   port: 587,
   secure: false,
-  auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_APP_PASSWORD },
+  auth: { user: process.env.BREVO_USER, pass: process.env.SMTP_KEY },
 });
 
 transporter.verify((error, success) => {
