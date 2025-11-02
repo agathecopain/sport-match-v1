64c62a418f1648227617f35328603aad48795d28
Agathe Boncompain <agathe.boncompain@gmail.com>
2025-11-02 17:46:34 +0100

    feat(sendEmail): new stmp port

--- back-end/src/utils/sendEmail.js
+++ back-end/src/utils/sendEmail.js
@@ -3,9 +3,17 @@ import "dotenv/config";
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  port:8000,
-  secure:false,
+  port: 587,
+  secure: false,
   auth: { user: process.env.GMAIL_USER, pass: process.env.GMAIL_APP_PASSWORD },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP connection failed", error);
+  } else {
+    console.log("SMTP connection OK", success);
+  }
 });
 
 export default async function sendEmail({ to, subject, html }) {
